f9dd9f948f8553998155be64448a7ac94018e874
Giacomo Licari <giacomo.licari@gmail.com>
2021-04-19 09:42:43 +0200

    Rename master branch to main, remove not needed content from deploy.sh script

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,15 +3,11 @@
 set -euo pipefail
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-if [ "$1" = "develop" -o "$1" = "main" ]; then # if using other branches, change branch names accordingly.
-    # If image does not exist, don't use cache
-    docker pull gnosispm/$DOCKERHUB_PROJECT:$1 && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 || \
-    docker build -t $DOCKERHUB_PROJECT -f docker/Dockerfile .
-else
-    docker pull gnosispm/$DOCKERHUB_PROJECT:staging && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:staging || \
-    docker build -t $DOCKERHUB_PROJECT -f docker/Dockerfile .
-fi
+
+# If image does not exist, don't use cache
+docker pull gnosispm/$DOCKERHUB_PROJECT:$1 && \
+docker build -t $DOCKERHUB_PROJECT -f docker/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 || \
+docker build -t $DOCKERHUB_PROJECT -f docker/Dockerfile .
+
 docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
 docker push gnosispm/$DOCKERHUB_PROJECT:$1
